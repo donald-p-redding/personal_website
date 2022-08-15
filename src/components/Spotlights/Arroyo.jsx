e6879f9d59e0663a33b4e473c7232ae49ef02ccf
Chip Dean <xxchipdeanxx@gmail.com>
2022-08-14 17:33:50 -0700

    added SVG + learn more button to arroyo spotlight

--- src/components/Spotlights/Arroyo.jsx
+++ src/components/Spotlights/Arroyo.jsx
@@ -3,17 +3,17 @@ function Arroyo () {
     <>
       <div class="arroyo">
           <div class="center">
-            <h2>Arroyo</h2>
-            <img alt="arroyo logo" src="images/spotlight/arroyographic_color.png" />
+            <img alt="arroyo logo" src="images/spotlight/arroyologo_color_dark_bg.svg" />
           </div>
           <div class="snippet">
             <p>Arroyo allows software engineers to query logs archived in cloud storage and conditionally reignest the matching logs back into their ELK stack.</p>
-            <img src="images/portfolio/bulk_10fps.gif"></img>
+            <img alt="Arroyo User Interface" src="images/portfolio/bulk_10fps.gif"></img>
           </div>
           <div class="snippet">
             <p>Arroyo automatically deploys and destroys the necessary AWS infrastructure to perform rehydration tasks. Once a rehydration job is complete, newly indexed logs are searchable in a user's Kibana dashboard.</p>
-            <img src="images/portfolio/deploy.gif"></img>
+            <img alt="Arroyo Deployment Script" src="images/portfolio/deploy.gif"></img>
           </div>
+          <a rel="noreferrer" target="_blank" href="https://www.arroyoframework.com/case-study.html" className="btn btn-border-light btn-lg">Learn More</a>
       </div>
     </>
   )
