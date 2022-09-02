54258ef0c610e970e560cb4ff86f4716e4f293cf
Chip Dean <xxchipdeanxx@gmail.com>
2022-09-01 18:37:27 -0700

    added content to ArroyoContent component

--- src/components/Spotlights/Arroyo.jsx
+++ src/components/Spotlights/Arroyo.jsx
@@ -1,22 +1,29 @@
+import ArroyoContent from "./ArroyoContent"; 
+
 function Arroyo () {
   return (
-    <>
-      <div class="arroyo">
-          <div class="center">
-            <img alt="arroyo logo" src="images/spotlight/arroyologo_color_dark_bg.svg" />
-          </div>
-          <div class="snippet">
-            <p>Arroyo allows software engineers to query logs archived in cloud storage and conditionally reingest the matching logs back into their ELK stack.</p>
-            <img alt="Arroyo User Interface" src="images/portfolio/bulk_10fps.gif"></img>
-          </div>
-          <div class="snippet">
-            <p>Arroyo automatically deploys and destroys the necessary AWS infrastructure to perform rehydration tasks. Once a rehydration job is complete, newly indexed logs are searchable in a user's Kibana dashboard.</p>
-            <img alt="Arroyo Deployment Script" src="images/portfolio/deploy.gif"></img>
-          </div>
-          <a rel="noreferrer" target="_blank" href="https://www.arroyoframework.com/case-study.html" className="btn btn-border-light btn-lg">Learn More</a>
-      </div>
-    </>
+    <section id="arroyo">
+      <ArroyoContent />
+    </section>
+
   )
 }
+
+    // <>
+    //   <div class="arroyo">
+    //       <div class="center">
+    //         <img alt="arroyo logo" src="images/spotlight/arroyologo_color_dark_bg.svg" />
+    //       </div>
+    //       <div class="snippet">
+    //         <p>Arroyo allows software engineers to query logs archived in cloud storage and conditionally reingest the matching logs back into their ELK stack.</p>
+    //         <img alt="Arroyo User Interface" src="images/portfolio/bulk_10fps.gif"></img>
+    //       </div>
+    //       <div class="snippet">
+    //         <p>Arroyo automatically deploys and destroys the necessary AWS infrastructure to perform rehydration tasks. Once a rehydration job is complete, newly indexed logs are searchable in a user's Kibana dashboard.</p>
+    //         <img alt="Arroyo Deployment Script" src="images/portfolio/deploy.gif"></img>
+    //       </div>
+    //       <a rel="noreferrer" target="_blank" href="https://www.arroyoframework.com/case-study.html" className="btn btn-border-light btn-lg">Learn More</a>
+    //   </div>
+    // </>
 
 export default Arroyo;
---
+++ src/components/Spotlights/ArroyoContent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ArroyoContent () {
+  return (
+    <>
+      <p>
+        Arroyo integrates with a user's existing ELK stack to rehydrate logs archived in AWS S3 and re-ingests those logs back into Elasticsearch. Users can reingest logs by date range, and they may also input search terms to refine their search further.
+      </p>
+      <p>
+        I built Arroyo as part of a four-person remote team spanning three separate time zones intending to breathe new life into older data.
+        Whether to develop new metrics with some additional historical context or search logs in a feature-rich familiar environment for audit purposes, Arroyo can help derive value <em>when</em> needed.
+      </p>
+      <p>
+        Arroyo's infrastructure is easily deployed to AWS with a single command. Once rehydration jobs are completed, Arroyo can automatically destroy that infrastructure to avoid unnecessary resource use.
+      </p>
+      <a rel="noreferrer" target="_blank" href="https://www.arroyoframework.com/case-study.html" className="btn btn-border-light btn-lg">Read Case Study</a>
+    </>
+  )
+}
+
+export default ArroyoContent;
