16b6cb8ce8b6c01db0b1afe9ca6b4a4eb7c52632
Donny Redding <71610961+donald-p-redding@users.noreply.github.com>
2024-12-21 08:41:35 -0800

    Updates

--- static/js/4.3bb42036.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpbako=this.webpackJsonpbako||[]).push([[4],{101:function(t,e,i){"use strict";i.r(e),e.default=i.p+"static/media/how-to-become-better-with-ui-design.cb9cebf5.md"}}]);
-//# sourceMappingURL=4.3bb42036.chunk.js.map
--- static/js/6.d67c2fef.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpbako=this.webpackJsonpbako||[]).push([[6],{103:function(t,a,s){"use strict";s.r(a),a.default=s.p+"static/media/the-ugly-truth-about-design.a6c056de.md"}}]);
-//# sourceMappingURL=6.d67c2fef.chunk.js.map
