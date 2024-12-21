1d85990e6703d9fe29adc0d4dce6b15e78c54d77
Donny Redding <71610961+donald-p-redding@users.noreply.github.com>
2024-12-21 08:45:03 -0800

    initial push

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ import "./App.scss";
 
 import { BrowserRouter, Switch, Route, HashRouter as Router } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import BlogDetails from "./pages/BlogDetails";
-
-
 
 function App() {
   return (
-    <BrowserRouter>
       <Switch>
         <Route path="/" component={Homepage} exact />
+        <Route path="/*" component={Homepage} />
       </Switch>
-    </BrowserRouter>
   );
 }
 
