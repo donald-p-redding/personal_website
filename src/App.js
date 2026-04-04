7e67c132ed5b8d46e399ec1a3ca3c92deef84d7b
Donny Redding <71610961+donald-p-redding@users.noreply.github.com>
2026-04-04 06:48:53 -0700

    remove unused assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./App.scss";
 
 import { Switch, Route } from "react-router-dom";
